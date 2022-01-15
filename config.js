8f004168ea4bd6e3a2a250296dbb72342b377187
Glitch (charm-crawling-expert) <none>
2022-01-15 03:15:54 +0000

    🥟🔃 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-inline-comments */
+
+module.exports = {
+  port: 3000, // Pport to listen on
+  prefix: "#", // Prefix for commands
+  id: "766256361344204800", // Discord Bot ID
+  usingCustomDomain: true, // Use a custom domain for the bot
+  domain: "http://charm-crawling-expert.glitch.me", // Custom domain
+  discordInvite: "https://discord.gg/", // Discord Invite URL
+  mongodbUrl:
+    "mongodb+srv://suhaib:hydro786@cluster0.gjnvo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // MongoDB connection URL
+  clientSecret: "-bxsS1Kao-tDQUos7_s-DNJwn7kECnfP", // Client Secret from Discord Application
+  token: "NzY2MjU2MzYxMzQ0MjA0ODAw.X4gt2A.ATly7BehIBzObXnYgKmrzUDujWQ", // Bot Token from Discord Application
+};
+
+/**
+ * !!!
+ * You need to add a redirect url to https://discordapp.com/developers/applications/ID/oauth2.
+ * Format is: domain:port/callback example http://localhost:8080/callback
+ * - Do not include port if the port is 80.
+ * !!!
+ */
